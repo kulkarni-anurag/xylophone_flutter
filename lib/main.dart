2afad3855a48ec92ea471a9d0c9d364b8e7261ec
Anurag Kulkarni <kanurag0801@gmail.com>
2022-01-21 22:37:22 +0530

    UI and Sound Playing Complete

--- lib/main.dart
+++ lib/main.dart
@@ -8,13 +8,104 @@ void main() {
 class XylophoneApp extends StatelessWidget {
   const XylophoneApp({Key? key}) : super(key: key);
 
+  void playSound(int soundNumber) {
+    final player = AudioCache();
+    player.play('note$soundNumber.wav');
+  }
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
         body: SafeArea(
-          child: Center(
-            child: Text('Hello World!'),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: <Widget>[
+              Expanded(
+                child: TextButton(
+                  child: const Text('Click Me!'),
+                  style: ButtonStyle(
+                    backgroundColor:
+                        MaterialStateProperty.all<Color>(Colors.red),
+                  ),
+                  onPressed: () {
+                    playSound(1);
+                  },
+                ),
+              ),
+              Expanded(
+                child: TextButton(
+                  child: const Text('Click Me!'),
+                  style: ButtonStyle(
+                    backgroundColor:
+                        MaterialStateProperty.all<Color>(Colors.orange),
+                  ),
+                  onPressed: () {
+                    playSound(2);
+                  },
+                ),
+              ),
+              Expanded(
+                child: TextButton(
+                  child: const Text('Click Me!'),
+                  style: ButtonStyle(
+                    backgroundColor:
+                        MaterialStateProperty.all<Color>(Colors.yellow),
+                  ),
+                  onPressed: () {
+                    playSound(3);
+                  },
+                ),
+              ),
+              Expanded(
+                child: TextButton(
+                  child: const Text('Click Me!'),
+                  style: ButtonStyle(
+                    backgroundColor:
+                        MaterialStateProperty.all<Color>(Colors.green),
+                  ),
+                  onPressed: () {
+                    playSound(4);
+                  },
+                ),
+              ),
+              Expanded(
+                child: TextButton(
+                  child: const Text('Click Me!'),
+                  style: ButtonStyle(
+                    backgroundColor:
+                        MaterialStateProperty.all<Color>(Colors.teal),
+                  ),
+                  onPressed: () {
+                    playSound(5);
+                  },
+                ),
+              ),
+              Expanded(
+                child: TextButton(
+                  child: const Text('Click Me!'),
+                  style: ButtonStyle(
+                    backgroundColor:
+                        MaterialStateProperty.all<Color>(Colors.blue),
+                  ),
+                  onPressed: () {
+                    playSound(6);
+                  },
+                ),
+              ),
+              Expanded(
+                child: TextButton(
+                  child: const Text('Click Me!'),
+                  style: ButtonStyle(
+                    backgroundColor:
+                        MaterialStateProperty.all<Color>(Colors.purple),
+                  ),
+                  onPressed: () {
+                    playSound(7);
+                  },
+                ),
+              ),
+            ],
           ),
         ),
       ),
